59ed64f2eda1ff4652d732a342fa75b2fa150eef
Joey <joeyynchan@gmail.com>
2016-07-10 17:08:26 -0400

    Tidying up UI
    updated background.js

--- src/background.js
+++ src/background.js
@@ -1,6 +1,3 @@
 function countdown(s) {
-  setTimeout(function(){ alert("Mister, your noodle is done;)"); }, s*1000);
+  setTimeout(function(){ alert("Mister, your noodle is done"); }, s*1000);
 }
-
-countdown(3);
-console.log("HI");
--- src/popup.js
+++ src/popup.js
@@ -1,20 +1,9 @@
-function countdown(s) {
-  setTimeout(function(){ alert("Mister, your noodle is done;)"); }, s*1000);
+function submitTimer() {
+  var s = document.querySelector("#duration").value;
+   background.countdown(s);
 }
 
-function createalarm(){
- chrome.alarms.create("My First Alarm",{delayInMinutes:0.1});
- chrome.alarms.get("My First Alarm",function(alarm){
-  console.log("Scheduled Time  "+ alarm.scheduledTime);
-  console.log("Alarm Name "+alarm.name);
- });
- chrome.alarms.onAlarm.addListener(function(alarm){
-  console.log("Alarm Elapsed Name "+alarm.name);
-  console.log("This is Over");
-  chrome.alarms.clear("My First Alarm");
-  alert(" Alarms Cleared");
- });
- //chrome.alarms.clear("My First Alarm");
-}
-
-window.onload=createalarm;
+var background = chrome.extension.getBackgroundPage();
+window.addEventListener('load', function(evt) {
+    document.getElementById('form').addEventListener('submit', submitTimer);
+});
